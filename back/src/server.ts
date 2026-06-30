b07dadcd7b8413a16c765ab0745911da265232cc
Salino3 <flaviobcn012@gmail.com>
2026-06-30 12:40:20 +0200

    server running

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,1 +1,21 @@
-console.log("Hi!");
+import express from "express";
+import path from "path";
+import { PORT } from "./constants";
+
+const app = express();
+
+// Base request parsers middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve assets folder physically stored on server locally
+app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
+
+// General health check endpoint
+app.get("/health", (req, res) => {
+  res.json({ status: "online", project: "Invenio Nexus Backend" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server up and running at http://localhost:${PORT}`);
+});

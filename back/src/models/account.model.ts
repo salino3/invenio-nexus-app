eb5dc7c01293473af8ec061a1aa16b18253b0517
Salino3 <flaviobcn012@gmail.com>
2026-07-02 10:54:16 +0200

    fix

--- back/src/models/account.model.ts
+++ back/src/models/account.model.ts
@@ -59,7 +59,7 @@ export class Account {
   }
 
   //
-  static async retrieveAllAccounts(): Promise<AllAccounts[] | null> {
+  static async retrieveAllAccounts(): Promise<AllAccounts[]> {
     const sql = `
       SELECT COALESCE(
      json_agg(to_jsonb(accounts) - '{password, created_at, updated_at, deleted_at}'::text[]), 
@@ -70,6 +70,6 @@ export class Account {
 
     const { rows } = await query(sql);
 
-    return rows;
+    return rows[0]?.accounts_list ?? [];
   }
 }

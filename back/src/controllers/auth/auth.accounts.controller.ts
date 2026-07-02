20327b4aada778ae21e2e661673b48993c47ed97
Salino3 <flaviobcn012@gmail.com>
2026-07-02 11:14:01 +0200

    fix

--- back/src/controllers/auth/auth.accounts.controller.ts
+++ back/src/controllers/auth/auth.accounts.controller.ts
@@ -3,6 +3,6 @@ import bcrypt from "bcryptjs";
 
 export class AuthController {
   public async loginAccount(req: Request, res: Response): Promise<Response> {
-    return res;
+    return res.status(200).send("ok");
   }
 }
--- back/src/routes/auth/auth.accounts.routes.ts
+++ back/src/routes/auth/auth.accounts.routes.ts
@@ -4,6 +4,6 @@ import { AuthController } from "../../controllers/auth/auth.accounts.controller"
 const routerAuth = express.Router();
 const routeController = new AuthController();
 
-routerAuth.post("login-account", routeController.loginAccount);
+routerAuth.post("/login-account", routeController.loginAccount);
 
 export default routerAuth;

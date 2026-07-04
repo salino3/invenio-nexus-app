4e89df42662a4180c60958993bfaca1ed167565d
Salino3 <flaviobcn012@gmail.com>
2026-07-04 15:59:43 +0200

    save changes

--- front/src/pods/public-dashboard/public-dashboard.component.tsx
+++ front/src/pods/public-dashboard/public-dashboard.component.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import type React from "react";
+import { LoginForm } from "./components";
 import "./public-dashboard.styles.scss";
 
 export const PublicDashboard: React.FC = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+
   return (
     <div className="rootPublicDashboard">
       <h1>PublicDashboardLayout</h1>
+
+      <div className="boxButtonsPage">
+        <button onClick={() => setIsLogin(true)}>Login</button>
+        <button onClick={() => setIsLogin(false)}>Register</button>
+      </div>
+
+      {isLogin ? <LoginForm /> : "Register component"}
     </div>
   );
 };

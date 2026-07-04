720a252518e17ac6b72af6ae07e2327567af0e36
Salino3 <flaviobcn012@gmail.com>
2026-07-04 15:35:35 +0200

    create first pod

--- front/src/layouts/public-dashboard/public-dashboard.layout.tsx
+++ front/src/layouts/public-dashboard/public-dashboard.layout.tsx
@@ -3,7 +3,7 @@ import "./public-dashboard.styles.scss";
 const PublicDashboardLayout: React.FC = () => {
   return (
     <div className="rootPublicDashboardLayout">
-      <h1>PublicDashboardLayout</h1>
+      <PublicDashboard />
     </div>
   );
 };
---
+++ front/src/pods/public-dashboard/pods/public-dashboard.component.tsx
@@ -0,0 +1,9 @@
+import "./public-dashboard.styles.scss";
+
+export const PublicDashboard: React.FC = () => {
+  return (
+    <div className="rootPublicDashboard">
+      <h1>PublicDashboardLayout</h1>
+    </div>
+  );
+};

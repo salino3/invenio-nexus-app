d4f087065ca670d1fe32125519b8eba52de24b83
Salino3 <flaviobcn012@gmail.com>
2026-07-06 17:23:39 +0200

    refactoring

--- front/src/router/session-routes/private-routes.tsx
+++ front/src/router/session-routes/private-routes.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { Outlet, useNavigate, Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { useProviderSelector } from "@/store/provider";
 import { utilitiesApp } from "@/utils";
 import { routePaths } from "../routes.interface";
 
 export const PrivateRoutes: React.FC = () => {
-  const navigate = useNavigate();
   const { setDataUser } = useProviderSelector("setDataUser");
 
   const { getAuthToken } = utilitiesApp();
 
   const token = getAuthToken();
   React.useEffect(() => {
-    if (!token) {
-      navigate(routePaths.public_dashboard);
-    } else {
+    if (token) {
       setDataUser && setDataUser(token);
     }
   }, []);

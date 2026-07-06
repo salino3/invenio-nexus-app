9e37d327795d88f7602c9e25556be9d4120b0998
Salino3 <flaviobcn012@gmail.com>
2026-07-06 16:28:43 +0200

    change name file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import AppRouter from "./router/AppRouter";
+import AppRouter from "./router/app-router";
 import { useProvider } from "./store/provider";
 
 function App() {

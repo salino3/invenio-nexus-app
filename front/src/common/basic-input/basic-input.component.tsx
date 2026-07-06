fcb934fd50294f9a014d1d380a4cf3f94c336b76
Salino3 <flaviobcn012@gmail.com>
2026-07-06 15:20:30 +0200

    create custom input component

---
+++ front/src/common/basic-input/basic-input.component.tsx
@@ -0,0 +1,35 @@
+import type React from "react";
+import "./basic-input.styles.scss";
+
+interface Props {
+  value?: string | number | readonly string[] | undefined;
+  stateValue?: string | number | readonly string[] | undefined;
+  change?: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  name: string | undefined;
+  type: React.HTMLInputTypeAttribute | undefined;
+  lbl?: string;
+  customStyles?: string;
+  errorMsg: string;
+}
+
+export const BasicInput: React.FC<Props> = (props) => {
+  const { value, stateValue, change, name, type, lbl, customStyles, errorMsg } =
+    props;
+
+  return (
+    <div className={`rootBasicInput ${customStyles}`}>
+      <label htmlFor={name + "ID"}>{lbl ?? name}</label>
+      <input
+        type={type}
+        id={name + "ID"}
+        name={name}
+        defaultValue={stateValue}
+        value={value}
+        onChange={change}
+      />
+      <div className="errorBox">
+        {errorMsg && <span className="errorMsg">{errorMsg}</span>}
+      </div>
+    </div>
+  );
+};

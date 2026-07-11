5fe1312fbc6e739210833fb828fe1157a6d83379
Salino3 <flaviobcn012@gmail.com>
2026-07-11 11:50:27 +0200

    fix

--- back/src/routes/subscriptions.router.ts
+++ back/src/routes/subscriptions.router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { stripeServices, StripeServices } from "../services/stripe-services";
+import { stripeServices } from "../services/stripe-services";
 
 const routerSubscriptions = Router();
 
 // POST /api/v1/subscriptions/create-payment-intent
 routerSubscriptions.post(
   "/create-payment",
-  stripeServices.createPaymentIntent.bind(StripeServices),
+  stripeServices.createPaymentIntent.bind(stripeServices),
 );
 
 export default routerSubscriptions;

9db5c88bf2d84f4633e96552ae3137433feb4d89
Salino3 <flaviobcn012@gmail.com>
2026-07-20 16:01:56 +0200

    add validations error form

--- front/src/utils/accounts/register-account.event.tsx
+++ front/src/utils/accounts/register-account.event.tsx
@@ -1,4 +1,17 @@
-import type { FormRegisterProps, StateRegisterAccount } from "./interface";
+import { regexCorrectEmail } from "../utilities-app";
+import type {
+  FormRegisterErrorsProps,
+  FormRegisterProps,
+  StateRegisterAccount,
+} from "./interface";
+
+let accountErrorData: FormRegisterErrorsProps = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  age: "",
+};
 
 export async function registerAccountEvent(
   prevState: StateRegisterAccount,
@@ -11,6 +24,53 @@ export async function registerAccountEvent(
     age: formData.get("age") as number | null,
     confirmPassword: formData.get("email") as string,
   };
+
+  (Object.entries(accountData) as [keyof FormRegisterProps, string][]).forEach(
+    ([key, value]) => {
+      if (!value || !value.trim()) {
+        accountErrorData = {
+          ...accountErrorData,
+          [key]: `The ${key} is required`,
+        };
+      } else {
+        if (key === "email") {
+          if (!regexCorrectEmail.test(value)) {
+            accountErrorData = {
+              ...accountErrorData,
+              [key]: "Invalid Email address",
+            };
+          }
+        } else if (key === "password") {
+          if (value.length < 6 || value.length > 20) {
+            accountErrorData = {
+              ...accountErrorData,
+              [key]: "Password must be between 6 and 20 characters",
+            };
+          }
+        } else if (key === "age" && typeof value === "number") {
+          if (value < 18) {
+            accountErrorData = {
+              ...accountErrorData,
+              [key]: "Your age must be more than 17 years",
+            };
+          }
+        }
+      }
+    },
+  );
+
+  const hasErrors: boolean = Object.values(accountErrorData).some(
+    (msg) => msg !== "",
+  );
+
+  if (hasErrors) {
+    return {
+      ...prevState,
+      fieldErrors: accountErrorData,
+      formData: accountData,
+    };
+  }
+
   try {
     return {
       success: true,

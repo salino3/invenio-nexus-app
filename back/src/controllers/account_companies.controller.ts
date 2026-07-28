bfe2f65ae6ffee7b6f510604e197d461a05d4062
Salino3 <flaviobcn012@gmail.com>
2026-07-28 10:08:17 +0200

    add validations

--- back/src/controllers/account_companies.controller.ts
+++ back/src/controllers/account_companies.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { AccountCookie } from "../interfaces/account.interface";
 
 export class AccountCompaniesController {
   //
@@ -6,6 +7,36 @@ export class AccountCompaniesController {
     req: Request,
     res: Response,
   ): Promise<Response> {
+    const { id } = (req.user || {}) as AccountCookie;
+
+    if (!id) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Missing account ID" });
+    }
+
+    const { uuidCompany } = req.params;
+
+    if (!uuidCompany) {
+      return res.status(400).send("Missing company UUID");
+    }
+
+    const { role } = req.body;
+
+    if (!role) {
+      return res
+        .status(401)
+        .json({ message: "Missing new role for updating account role" });
+    }
+
+    try {
+    } catch (error) {
+      console.error("Error executing updateCompanyByUUID endpoint:", error);
+      return res
+        .status(500)
+        .json({ error: "Internal server error during update" });
+    }
+
     return res;
   }
 }

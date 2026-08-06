f0c0325ebe3c9eafa8da06a2914c3cf9b9e8a27a
Salino3 <flaviobcn012@gmail.com>
2026-08-06 10:23:24 +0200

    add useState

--- front/src/pods/dashboard/dashboard.component.tsx
+++ front/src/pods/dashboard/dashboard.component.tsx
@@ -1,12 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ServicesApp } from "@/store/services";
 import "./dashboard.styles.scss";
 
+export interface SearchValuesCompaniesProps {
+  search: string;
+  offset: number;
+}
+
 export const Dashboard: React.FC = () => {
+  const [searchValuesCompanies, setSearchValuesCompanies] =
+    useState<SearchValuesCompaniesProps>({
+      search: "",
+      offset: 0,
+    });
+
   useEffect(() => {
-    ServicesApp.getSearchingCompanies("").then((res) =>
-      console.log("clog1", res),
-    );
+    ServicesApp.getSearchingCompanies(
+      searchValuesCompanies.search,
+      searchValuesCompanies.offset,
+    ).then((res) => console.log("clog1", res));
   }, []);
 
   return <div className="rootDashboard">DashboardLayout</div>;

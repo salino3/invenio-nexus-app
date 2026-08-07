90b9e496a65cf9f6c7240035e898ea86e098fb08
Salino3 <flaviobcn012@gmail.com>
2026-08-07 11:00:16 +0200

    add redirection text

--- front/src/pods/dashboard/components/card-company-list/card-company-list.component.tsx
+++ front/src/pods/dashboard/components/card-company-list/card-company-list.component.tsx
@@ -12,14 +12,23 @@ export const CardCompanyList: React.FC<Props> = ({ company }) => {
   return (
     <li className="rootCardCompanyList cleanList">
       <div className="containerCardUp">
-        <h4>{company.name}</h4>
-        <ImageComponent
-          vertical={false}
-          src={`${VITE_URL_BACK_FILE}${company.logo ?? ""}`}
-          lazy={"lazy"}
-          alt={company.name}
-          customStyle="boxImage"
-        />
+        <div className="boxLeft">
+          <h4>{company.name}</h4>
+          <span>{company.sector}</span>
+        </div>
+        <div className="boxCenter">
+          <span>{company.location}</span>
+          <span>{company.country_code}</span>
+        </div>
+        <div className="boxRight">
+          <ImageComponent
+            vertical={false}
+            src={`${VITE_URL_BACK_FILE}${company.logo ?? ""}`}
+            lazy={"lazy"}
+            alt={company.name}
+            customStyle="boxImage"
+          />
+        </div>
       </div>
       <div className="containerCardDown">
         {company.hashtags &&
@@ -27,6 +36,12 @@ export const CardCompanyList: React.FC<Props> = ({ company }) => {
           company.hashtags.map((h: string) => (
             <span className="hashtag">#{h}</span>
           ))}
+        <span
+          className="redirectionText"
+          onClick={() => alert("TODO: redirection page..")}
+        >
+          More info..
+        </span>
       </div>
     </li>
   );

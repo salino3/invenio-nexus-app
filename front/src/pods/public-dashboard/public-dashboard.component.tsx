552d5446bfbb381ff8edf87fa37ca370557d167e
Salino3 <flaviobcn012@gmail.com>
2026-08-14 09:51:58 +0200

    add modal with message

--- front/src/pods/public-dashboard/public-dashboard.component.tsx
+++ front/src/pods/public-dashboard/public-dashboard.component.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import { ModalApp } from "@/common-app";
 import { LoginForm, RegisterForm } from "./components";
 import { VITE_URL_BACK } from "@/constants";
 import "./public-dashboard.styles.scss";
 
 export const PublicDashboard: React.FC = () => {
   const [isLogin, setIsLogin] = useState<boolean>(true);
-
+  const [showModal, setShowModal] = useState<boolean>(false);
   const loginWithGoogle = () => {
     window.location.href = `${VITE_URL_BACK}/auth/google`;
   };
@@ -19,7 +20,18 @@ export const PublicDashboard: React.FC = () => {
         <button onClick={() => setIsLogin(false)}>Register</button>
       </div>
 
-      {isLogin ? <LoginForm /> : <RegisterForm setIsLogin={setIsLogin} />}
+      {isLogin ? (
+        <LoginForm />
+      ) : (
+        <RegisterForm setShowModal={setShowModal} setIsLogin={setIsLogin} />
+      )}
+      <ModalApp
+        setShowModal={setShowModal}
+        showModal={showModal}
+        title="Registration message"
+      >
+        <strong>Registration successfully completed</strong>
+      </ModalApp>
     </div>
   );
 };

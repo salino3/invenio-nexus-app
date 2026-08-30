4caeffc548f4d9f71cc1066c7110e205b0d15532
Salino3 <flaviobcn012@gmail.com>
2026-08-30 15:15:51 +0200

    new pod component

--- front/src/layouts/company-page/company-page.layout.tsx
+++ front/src/layouts/company-page/company-page.layout.tsx
@@ -1,8 +1,14 @@
 import type React from "react";
+import { CompanyPage } from "@/pods";
 import "./company-page.styles.scss";
 
 const CompanyPageLayout: React.FC = () => {
-  return <div className="rootCompanyPageLayout">Hi</div>;
+  return (
+    <div className="rootCompanyPageLayout">
+      {" "}
+      <CompanyPage />
+    </div>
+  );
 };
 
 export default CompanyPageLayout;
---
+++ front/src/pods/company-page/company-page.component.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import "./company-page.styles.scss";
+
+export const CompanyPage: React.FC = () => {
+  return <div className="rootCompanyPage">hi</div>;
+};

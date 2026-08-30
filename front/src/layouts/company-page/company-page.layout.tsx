b9d8cdcd8ace5eebdfcb07c29f675afe62c75bca
Salino3 <flaviobcn012@gmail.com>
2026-08-30 15:02:48 +0200

    create layout component

---
+++ front/src/layouts/company-page/company-page.layout.tsx
@@ -0,0 +1,8 @@
+import type React from "react";
+import "./company-page.styles.scss";
+
+const CompanyPageLayout: React.FC = () => {
+  return <div className="rootCompanyPageLayout"></div>;
+};
+
+export default CompanyPageLayout;

8a5562e78d6283dcdb06d8a97732526ae6baf5ce
Salino3 <flaviobcn012@gmail.com>
2026-09-07 16:12:03 +0200

    add interface account favorites

---
+++ back/src/models/account_favorite.model.ts
@@ -0,0 +1,17 @@
+// models/account-favorite.model.ts
+
+import { query } from "../db";
+import { QueryResult } from "pg";
+import { AccountFavoritesProps } from "../interfaces/account_favorite.interface";
+
+export class AccountFavorites {
+  public account_id: number;
+  public company_uuid: string;
+  public created_at: Date;
+
+  constructor(data: AccountFavoritesProps) {
+    this.account_id = data.account_id;
+    this.company_uuid = data.company_uuid;
+    this.created_at = data.created_at;
+  }
+}

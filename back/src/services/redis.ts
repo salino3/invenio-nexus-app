442b8156da33f51df4a3f23470bf876a1b766664
Salino3 <flaviobcn012@gmail.com>
2026-09-10 12:10:01 +0200

    add import

--- back/src/services/redis.ts
+++ back/src/services/redis.ts
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
+import { REDIS_URL } from "../constants";
+// docker run -d --name my-redis -p 6379:6379 redis:alpine
 
 export const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: REDIS_URL || "redis://localhost:6379",
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
